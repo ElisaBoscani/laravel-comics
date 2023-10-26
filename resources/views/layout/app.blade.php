9703acebc96e237bc8b4f2c1ce209fc2b9a9a802
Elisa Boscani <elisabosc90@gmail.com>
2023-10-26 17:02:16 +0200

    creato il footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -23,7 +23,7 @@
     <main class="bg-light">
         @yield('content')
     </main>
-
+    @include('partials.footer')
 </body>
 
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,77 @@
+<div class="margin_top">
+  <div class="d-flex justify-content-between bg_img">
+    <div class="d-flex gap-5 container col p-4 marg_top_col">
+      <!-- comisc -->
+      <div>
+        <div>
+          <h3 class="text-white text-uppercase">DC Comics</h3>
+          <ul class="p-0">
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+        <!-- shop -->
+        <div>
+          <h3 class="text-white text-uppercase">Sites</h3>
+          <ul class="p-0">
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- dc -->
+      <div>
+        <h3 class="text-white text-uppercase">DC</h3>
+        <ul class="p-0">
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy Policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <!-- sites -->
+      <div>
+        <h3 class="text-white text-uppercase">Sites</h3>
+        <ul class="p-0">
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universal</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col pb-4 img_logo"></div>
+  </div>
+</div>
+
+
+<!-- footer botton -->
+<div class="footer_botton">
+  <div class="d-flex justify-content-between container p-3">
+    <div class="border border-2 border-primary p-2">
+      <a href="#" class="text-uppercase fw-bolder"> Sing-up now!</a>
+    </div>
+    <div class="d-flex justify-content-evenly w-50 align-items-center">
+      <a href="#" class="follow fs-5 text-uppercase fw-bolder">Follow us</a>
+      <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="" />
+      <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="" />
+      <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="" />
+      <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="" />
+      <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="" />
+    </div>
+  </div>
+</div>
