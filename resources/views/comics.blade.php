ec5f32bf41d954a0bd6ad6528653e66d64427f78
Elisa Boscani <elisabosc90@gmail.com>
2023-10-26 18:35:13 +0200

    create le card dei fumetti

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-<h1>Comics</h1>
+<div id="main_comics">
+  <div class="container">
+    <h2 class="my-4 text-white ">Curret serie</h2>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4">
+      @foreach ($comics as $indice => $comic)
+      <div class="col h-100 ">
 
+        <img class="img-fluid card-img-top" src="{{ $comic['thumb'] }}" alt="">
+        <div class="pt-3">
+          <h4 class=" text-white ">
+            {{ $comic['title'] }}
+          </h4>
+        </div>
+
+      </div>
+      @endforeach
+    </div>
+  </div>
+</div>
 @endsection
